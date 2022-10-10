b14b4735540392ae0032bedb744bb658f1fa8dc8
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-10-10 22:26:50 +0200

    Update Bi_streamlit.py

--- Bi_streamlit.py
+++ Bi_streamlit.py
@@ -54,7 +54,7 @@ with col5:
 with col6:
    st.write('')
 
-if (uploaded_file is None) and (list_lines='Ex: google.com, 12335, DIRECT'):
+if (uploaded_file is None) and (list_lines=='Ex: google.com, 12335, DIRECT'):
     st.write('Please insert input')
 
 # Create API client.
