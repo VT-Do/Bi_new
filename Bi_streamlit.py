68a8c21c06abd735c6542b8bca9a02dc5f0804ac
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-10-10 18:36:56 +0200

    Update Bi_streamlit.py

--- Bi_streamlit.py
+++ Bi_streamlit.py
@@ -30,9 +30,9 @@ if uploaded_file is not None:
     # Can be used wherever a "file-like" object is accepted:
     input = pd.read_csv(uploaded_file, header=None)
 	
-    advertisingsystem=input[0].str.replace(' ', '')
-    pubaccid=input[1].astype('string').str.replace(' ', '')
-    relationship=input[2].str.replace(' ', '')
+    advertisingsystem=tuple(input[0].str.replace(' ', ''))
+    pubaccid=tuple(input[1].astype('string').str.replace(' ', ''))
+    relationship=tuple(input[2].str.replace(' ', ''))
 
     st.sidebar.write('Uploaded data',input)
 	
