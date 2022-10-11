f33d1a87a9d311b015aac96f1f84286a594dec4e
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-10-11 14:29:55 +0200

    Update Bi_streamlit.py

--- Bi_streamlit.py
+++ Bi_streamlit.py
@@ -52,11 +52,11 @@ if choice2=='Upload':
 	
 	
 	if check(upload_input,2,'DIRECT|RESELLER'):
-    		print('Check Relationship:')
-    		print(check(upload_input,2,'DIRECT|RESELLER'))
-	if check(upload_input,0,'..'):
-    		print('Check AdvertisingSystem:')
-    		print(check(upload_input,0,'.'))
+    		st.sidebar.write('Check Relationship:')
+    		st.sidebar.write(check(upload_input,2,'DIRECT|RESELLER'))
+	if check(upload_input,0,'.'):
+    		st.sidebar.write('Check AdvertisingSystem:')
+    		st.sidebar.write(check(upload_input,0,'.'))
 	
         st.sidebar.table('Uploaded data',upload_input)
 	
