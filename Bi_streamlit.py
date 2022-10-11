2bbaf7d2905b79672d0ed031781e067dd58da74b
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-10-11 14:42:25 +0200

    Update Bi_streamlit.py

--- Bi_streamlit.py
+++ Bi_streamlit.py
@@ -27,7 +27,7 @@ uploaded_file=None
 list_lines='Ex: google.com, 12335, DIRECT'
 
 
-choice = st.sidebar.radio("Select invironment",('WEB','APP','TESTT'), horizontal=True)
+choice = st.sidebar.radio("Select invironment",('WEB','APP','TESTTT'), horizontal=True)
 
 
 choice2 = st.sidebar.radio("Insert input",('Upload','Type/Paste'), horizontal=True)
