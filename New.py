3e3cffb2151ec9762fcd352ae67428494d468ae7
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-10-12 17:58:25 +0200

    Update New.py

--- New.py
+++ New.py
@@ -44,7 +44,7 @@ def download(output_data):
 	
         st.dataframe(output_data.reset_index(drop=True))
     else:
-        st.write(' \n No output found')
+        st.write(' \n  \n   \n No output found')
 	
 
 	
