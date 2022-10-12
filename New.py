fe04be795a8c912ee71ad7026bab4d09a310aabf
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-10-12 22:28:12 +0200

    Update New.py

--- New.py
+++ New.py
@@ -42,7 +42,7 @@ def download(output_data):
     		mime='text/csv',
 		)
 	
-        st.dataframe(output_data.reset_index(drop=True),200,100)
+        st.dataframe(output_data.reset_index(drop=True),2000,1000)
 
     else:
         st.write('')
