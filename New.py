3211c806b06e2368cd5b47c54ca0b22a4ead4b38
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-10-18 17:53:22 +0200

    Update New.py

--- New.py
+++ New.py
@@ -117,30 +117,42 @@ if (uploaded_file is None) and (list_lines=='Ex: google.com, 12335, DIRECT'):
     st.markdown(f'<h1 style="color:#de4b4b;font-size:15px;">{"Please insert input!"}</h1>', unsafe_allow_html=True)
 
 # Create API client.
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"]
-)
+credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"])
 client = bigquery.Client(credentials=credentials)
 
-@st.cache
-def load_data1(): 
+
+
+if ('Time1' not in st.session_state) and ('Time2' not in st.session_state):
+    query_time1="SELECT A FROM `showheroes-bi.bi.Test` limit 1"
+    df_time1= client.query(query_time1).to_dataframe()
+    st.session_state['Time1']=df_time1['A'][0]
+
+    query_time2="SELECT Date FROM `showheroes-bi.bi.bi_appadstxt_join_sellersjson_with_count_domains` limit 1"
+    df_time2= client.query(query_time2).to_dataframe()
+    st.session_state['Time2']=df_time2['Date'][0]
+
+	
+
+@st.cache(max_entries=1)
+def load_data1(time): 
     query1="SELECT * FROM `showheroes-bi.bi.bi_adstxt_join_sellerjson_with_count_domains`"
     query_job1 = client.query(query1)
     return client.query(query1).to_dataframe().fillna('-')
 
 
 
-@st.cache
-def load_data2():
+@st.cache(max_entries=1)
+def load_data2(time):
     query2="SELECT * FROM `showheroes-bi.bi.bi_appadstxt_join_sellersjson_with_count_domains`"
     query_job2 = client.query(query2)
     return client.query(query2).to_dataframe().fillna('-')
 
 	
-df1=load_data1().copy()
-df2=load_data2().copy()
+df1=load_data1(st.session_state['Time1']).copy()
+df2=load_data2(st.session_state['Time2']).copy()
 
-
+st.write(st.session_state['Time1'])
+st.write(st.session_state['Time1'])
 if (choice=="WEB") and (uploaded_file is not None):
     # first filter before looping
     df1=df1[(df1['AdvertisingSystem'].isin(upload_input[0])) & (df1['PubAccId'].isin(upload_input[1]))]
