73492176ee887878f3a83f0144460991643753e2
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-10-24 17:58:04 +0200

    Update backup_v2.py

--- backup_v2.py
+++ backup_v2.py
@@ -273,6 +273,7 @@ if st.session_state.text_input =='BI_team':
         st.write(res)
         st.write((st.session_state["cache_updated"]))
 else:
-    st.button('Try again')
+    st.write('WRONG PASSWORD')
+   # st.button('Try again')
 #    text_input_container = st.empty()
  #   text_input_container.text_input("Enter Password", key="text_input")
