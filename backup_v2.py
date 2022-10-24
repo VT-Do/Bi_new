2d22e83681a579281fa03ea880d4f48f548d1e28
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-10-24 18:02:53 +0200

    Update backup_v2.py

--- backup_v2.py
+++ backup_v2.py
@@ -76,7 +76,7 @@ list_lines='Ex: google.com, 12335, DIRECT'
 text_input_container = st.empty()
 text_input_container.text_input("Enter Password", key="text_input")
 
-if (st.session_state.text_input != "") and (st.session_state.text_input != 'BI_team'):
+if (st.session_state.text_input != ""):
     st.write('Wrong password')
     text_input_container.empty()
     #st.info(st.session_state.text_input)
