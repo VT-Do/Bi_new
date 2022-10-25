04bd1c37e2ef296ee70778f1136b054b3ad1201d
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-10-25 15:28:24 +0200

    Update Test_v1.py

--- Test_v1.py
+++ Test_v1.py
@@ -40,15 +40,15 @@ if choice2=='Upload':
 	
 	
 	
-	if check(upload_input,2,'DIRECT|RESELLER'):
-    		st.sidebar.write('Check Relationship:')
-    		st.sidebar.write(check(upload_input,2,'DIRECT|RESELLER'))
-	if check(upload_input,0,'.'):
-    		st.sidebar.write('Check AdvertisingSystem:')
-    		st.sidebar.write(check(upload_input,0,'.'))
-	st.write('Nothing')
-        st.sidebar.table('Uploaded data',upload_input)
-	st.sidebar.table('Uploaded data',upload_input)
+        if check(upload_input,2,'DIRECT|RESELLER'):
+            st.sidebar.write('Check Relationship:')
+            st.sidebar.write(check(upload_input,2,'DIRECT|RESELLER'))
+        if check(upload_input,0,'.'):
+            st.sidebar.write('Check AdvertisingSystem:')
+            st.sidebar.write(check(upload_input,0,'.'))
+            st.write('Nothing')
+            st.sidebar.table('Uploaded data',upload_input)
+	    st.sidebar.table('Uploaded data',upload_input)
 	
 	
 
