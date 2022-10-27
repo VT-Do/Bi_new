ca2318955965ad7a2ae9b23a3d0a58ef809c1e46
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-10-27 20:55:01 +0200

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -262,7 +262,7 @@ elif st.session_state['authentication_status'] == None:
         st.write('')
     with col08:
         st.warning('Please enter your username and password')
-        st.write(username)
+     #   st.write(username)
     with col09:
         st.write('')
     	 	
