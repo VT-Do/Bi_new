52328e79e0e1350602e46c9abb427d368ce6635a
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-10-27 18:44:10 +0200

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -60,7 +60,7 @@ def download(output_data):
 	
 	
     
-if ('authentication_status' not in st.session_state):
+if ('Start' not in st.session_state):
     col01, col02,col03 = st.columns(3)
     with col01:
         st.write('')
