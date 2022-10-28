1ef8c904a3e43330e9ecdf1feedf5ac5726c0fcb
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-10-28 17:01:24 +0200

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -84,7 +84,7 @@ with col02:
         authenticator = stauth.Authenticate(config['credentials'],config['cookie']['name'],config['cookie']['key'],config['cookie']['expiry_days'],config['preauthorized'])		
         name, authentication_status, username = authenticator.login('Login', 'main')  
 with col03:
-    st.write('')
+    st.write(username)
 
 
 # initial setting
