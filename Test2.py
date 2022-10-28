667bf73db3dbdf551077dfcd6513938e2c9e370f
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-10-28 10:48:33 +0200

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -11,27 +11,39 @@ import time
 
 
 st.set_page_config(layout="wide")
+BACKGROUND_COLOR = 'white'
+COLOR = 'black'
 
-padding_top = 0
-padding_bottom = 10
-padding_left = 1
-padding_right = 10
-# max_width_str = f'max-width: 100%;'
-st.markdown(f'''
+def set_page_container_style(
+        max_width: int = 1100, max_width_100_percent: bool = False,
+        padding_top: int = 1, padding_right: int = 10, padding_left: int = 1, padding_bottom: int = 10,
+        color: str = COLOR, background_color: str = BACKGROUND_COLOR,
+    ):
+        if max_width_100_percent:
+            max_width_str = f'max-width: 100%;'
+        else:
+            max_width_str = f'max-width: {max_width}px;'
+        st.markdown(
+            f'''
             <style>
                 .reportview-container .sidebar-content {{
                     padding-top: {padding_top}rem;
                 }}
                 .reportview-container .main .block-container {{
+                    {max_width_str}
                     padding-top: {padding_top}rem;
                     padding-right: {padding_right}rem;
                     padding-left: {padding_left}rem;
                     padding-bottom: {padding_bottom}rem;
                 }}
+                .reportview-container .main {{
+                    color: {color};
+                    background-color: {background_color};
+                }}
             </style>
-            ''', unsafe_allow_html=True,
-)
-	
+            ''',
+            unsafe_allow_html=True,
+        )
 # col=0 (advertisingsystem), 1 (PubAccId) , 2 (Relationship),  
 def check(df,col,keyword):
     list=df[col][~df[col].str.contains(keyword)].tolist()
