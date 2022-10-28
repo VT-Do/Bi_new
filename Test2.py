c91131bf23ba895e4847e643f5809bd46540738a
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-10-28 11:08:57 +0200

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -11,39 +11,22 @@ import time
 
 
 st.set_page_config(layout="wide")
-BACKGROUND_COLOR = 'white'
-COLOR = 'black'
+padding_top = 0
 
-def set_page_container_style(
-        max_width: int = 1100, max_width_100_percent: bool = False,
-        padding_top: int = 1, padding_right: int = 10, padding_left: int = 1, padding_bottom: int = 10,
-        color: str = COLOR, background_color: str = BACKGROUND_COLOR,
-    ):
-        if max_width_100_percent:
-            max_width_str = f'max-width: 100%;'
-        else:
-            max_width_str = f'max-width: {max_width}px;'
-        st.markdown(
-            f'''
-            <style>
-                .reportview-container .sidebar-content {{
-                    padding-top: {padding_top}rem;
-                }}
-                .reportview-container .main .block-container {{
-                    {max_width_str}
-                    padding-top: {padding_top}rem;
-                    padding-right: {padding_right}rem;
-                    padding-left: {padding_left}rem;
-                    padding-bottom: {padding_bottom}rem;
-                }}
-                .reportview-container .main {{
-                    color: {color};
-                    background-color: {background_color};
-                }}
-            </style>
-            ''',
-            unsafe_allow_html=True,
-        )
+st.markdown(f"""
+    <style>
+        .reportview-container .main .block-container{{
+            padding-top: {padding_top}rem;
+        }}
+    </style>""",
+    unsafe_allow_html=True,
+)
+
+st.title("My Next Best Actions")
+
+st.sidebar.text_input("foo")
+
+
 # col=0 (advertisingsystem), 1 (PubAccId) , 2 (Relationship),  
 def check(df,col,keyword):
     list=df[col][~df[col].str.contains(keyword)].tolist()
