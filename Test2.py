b4fea18d474ca0f8f6680968fa65950c1fa977ce
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-10-28 09:20:37 +0200

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -60,7 +60,17 @@ def download(output_data):
 	
 	
     
-
+if ("AAA" not in st.session_state):
+    col01, col02,col03 = st.columns(3)
+    with col01:
+        st.write('')
+    with col02:
+        with open('config.yaml') as file:
+            config = yaml.safe_load(file)
+        authenticator = stauth.Authenticate(config['credentials'],config['cookie']['name'],config['cookie']['key'],config['cookie']['expiry_days'],config['preauthorized'])	
+        name, authentication_status, username = authenticator.login('Login', 'main')
+    with col03:
+        st.write('')
 
 
 # initial setting
