47d36960d54fec173b006e5283918b6be721a05f
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-11-02 18:34:30 +0100

    Update New.py

--- New.py
+++ New.py
@@ -144,109 +144,110 @@ if st.session_state["authentication_status"]:
 
     tab1, tab2 = container.tabs(["Main", "Contact"])
     with tab1:
-       st.write("Hello")
-    with tab2:
-       st.header("A dog")
+
+
  
 
-    if (uploaded_file is None) and ((list_lines=='Ex: google.com, 12335, DIRECT') or (list_lines.strip()=='')):
-        container.markdown(f'<h1 style="color:#de4b4b;font-size:15px;">{"Please insert input!"}</h1>', unsafe_allow_html=True)
+        if (uploaded_file is None) and ((list_lines=='Ex: google.com, 12335, DIRECT') or (list_lines.strip()=='')):
+            container.markdown(f'<h1 style="color:#de4b4b;font-size:15px;">{"Please insert input!"}</h1>', unsafe_allow_html=True)
 
-    # Create API client.
-    credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"])
-    client = bigquery.Client(credentials=credentials)
+        # Create API client.
+        credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"])
+        client = bigquery.Client(credentials=credentials)
 
 
 
-    if ('Time1' not in st.session_state) and ('Time2' not in st.session_state):
-        query_time1="SELECT Date FROM `showheroes-bi.bi.bi_adstxt_join_sellerjson_with_count_domains` limit 1"
-        df_time1= client.query(query_time1).to_dataframe()
-        st.session_state['Time1']=df_time1['Date'][0]
+        if ('Time1' not in st.session_state) and ('Time2' not in st.session_state):
+            query_time1="SELECT Date FROM `showheroes-bi.bi.bi_adstxt_join_sellerjson_with_count_domains` limit 1"
+            df_time1= client.query(query_time1).to_dataframe()
+            st.session_state['Time1']=df_time1['Date'][0]
 
-        query_time2="SELECT Date FROM `showheroes-bi.bi.bi_appadstxt_join_sellersjson_with_count_domains` limit 1"
-        df_time2= client.query(query_time2).to_dataframe()
-        st.session_state['Time2']=df_time2['Date'][0]
+            query_time2="SELECT Date FROM `showheroes-bi.bi.bi_appadstxt_join_sellersjson_with_count_domains` limit 1"
+            df_time2= client.query(query_time2).to_dataframe()
+            st.session_state['Time2']=df_time2['Date'][0]
 
 	
 
-    @st.cache(max_entries=1)
-    def load_data1(time): 
-        query1="SELECT * except(Date) FROM `showheroes-bi.bi.bi_adstxt_join_sellerjson_with_count_domains`"
-        query_job1 = client.query(query1)
-        return client.query(query1).to_dataframe().fillna('-')
+        @st.cache(max_entries=1)
+        def load_data1(time): 
+            query1="SELECT * except(Date) FROM `showheroes-bi.bi.bi_adstxt_join_sellerjson_with_count_domains`"
+            query_job1 = client.query(query1)
+            return client.query(query1).to_dataframe().fillna('-')
 
 
 
 
-    @st.cache(max_entries=1)
-    def load_data2(time):
-        query2="SELECT * except(Date) FROM `showheroes-bi.bi.bi_appadstxt_join_sellersjson_with_count_domains`"
-        query_job2 = client.query(query2)
-        return client.query(query2).to_dataframe().fillna('-')
+        @st.cache(max_entries=1)
+        def load_data2(time):
+            query2="SELECT * except(Date) FROM `showheroes-bi.bi.bi_appadstxt_join_sellersjson_with_count_domains`"
+            query_job2 = client.query(query2)
+            return client.query(query2).to_dataframe().fillna('-')
 	
-    df1=load_data1(st.session_state['Time1']).copy()
-    df2=load_data2(st.session_state['Time2']).copy()
+        df1=load_data1(st.session_state['Time1']).copy()
+        df2=load_data2(st.session_state['Time2']).copy()
 
 
-    if (choice=="WEB") and (uploaded_file is not None):
-        # first filter before looping
-        df1=df1[(df1['AdvertisingSystem'].isin(upload_input[0])) & (df1['PubAccId'].isin(upload_input[1]))]
-        df1=df1.reset_index(drop=True)
+        if (choice=="WEB") and (uploaded_file is not None):
+            # first filter before looping
+            df1=df1[(df1['AdvertisingSystem'].isin(upload_input[0])) & (df1['PubAccId'].isin(upload_input[1]))]
+            df1=df1.reset_index(drop=True)
 
-        # Initial setting
-        data1=pd.DataFrame(columns=df1.columns.tolist())
+            # Initial setting
+            data1=pd.DataFrame(columns=df1.columns.tolist())
 	
-        for row in range(upload_input.shape[0]):
-            data1=pd.concat([data1, check_row(df1,upload_input,row)]) 
+            for row in range(upload_input.shape[0]):
+                data1=pd.concat([data1, check_row(df1,upload_input,row)]) 
     
     
-        # Download 	
-        download(data1)
+            # Download 	
+            download(data1)
 	
-    elif (choice=="WEB") and (list_lines!='Ex: google.com, 12335, DIRECT') and (list_lines.strip()!=''):
-        # first filter 
-        df1=df1[(df1['AdvertisingSystem'].isin(input[0])) & (df1['PubAccId'].isin(input[1]))]
-        df1=df1.reset_index(drop=True)
+        elif (choice=="WEB") and (list_lines!='Ex: google.com, 12335, DIRECT') and (list_lines.strip()!=''):
+            # first filter 
+            df1=df1[(df1['AdvertisingSystem'].isin(input[0])) & (df1['PubAccId'].isin(input[1]))]
+            df1=df1.reset_index(drop=True)
     
-        data1=pd.DataFrame(columns=df1.columns.tolist())
+            data1=pd.DataFrame(columns=df1.columns.tolist())
 	
-        for row in range(input.shape[0]):
-            data1=pd.concat([data1, check_row(df1,input,row)]) 
-    
-
-        # Download 
-        download(data1)
-    
-	
-    elif (choice=="APP") and (uploaded_file is not None):   
-        # first filter 
-        df2=df2[(df2['AdvertisingSystem'].isin(upload_input[0])) & (df2['PubAccId'].isin(upload_input[1]))]
-        df2=df2.reset_index(drop=True)
-
-
-        # Initial setting
-        data2=pd.DataFrame(columns=df2.columns.tolist())
-	
-        for row in range(upload_input.shape[0]):
-            data2=pd.concat([data2, check_row(df2,upload_input,row)]) 
+            for row in range(input.shape[0]):
+                data1=pd.concat([data1, check_row(df1,input,row)]) 
     
 
-        # Download 	
-        download(data2)
+            # Download 
+            download(data1)
+    
+	
+        elif (choice=="APP") and (uploaded_file is not None):   
+            # first filter 
+            df2=df2[(df2['AdvertisingSystem'].isin(upload_input[0])) & (df2['PubAccId'].isin(upload_input[1]))]
+            df2=df2.reset_index(drop=True)
+
+
+            # Initial setting
+            data2=pd.DataFrame(columns=df2.columns.tolist())
+	
+            for row in range(upload_input.shape[0]):
+                data2=pd.concat([data2, check_row(df2,upload_input,row)]) 
+    
+
+            # Download 	
+            download(data2)
 
 	
-    elif (choice=="APP") and (list_lines!='Ex: google.com, 12335, DIRECT') and (list_lines.strip()!=''):
-        # first filter
-        df2=df2[(df2['AdvertisingSystem'].isin(input[0])) & (df2['PubAccId'].isin(input[1]))]
-        df2=df2.reset_index(drop=True)
+        elif (choice=="APP") and (list_lines!='Ex: google.com, 12335, DIRECT') and (list_lines.strip()!=''):
+            # first filter
+            df2=df2[(df2['AdvertisingSystem'].isin(input[0])) & (df2['PubAccId'].isin(input[1]))]
+            df2=df2.reset_index(drop=True)
 	
-        data2=pd.DataFrame(columns=df2.columns.tolist())
+            data2=pd.DataFrame(columns=df2.columns.tolist())
 	
-        for row in range(input.shape[0]):
-            data2=pd.concat([data2, check_row(df2,input,row)]) 
+            for row in range(input.shape[0]):
+                data2=pd.concat([data2, check_row(df2,input,row)]) 
 
-        # Download
-        download(data2)	
+            # Download
+            download(data2)	
+    with tab2:
+        st.header("A dog")
     
 elif st.session_state['authentication_status'] == False:
     col04, col05,col06 = st.columns(3)
