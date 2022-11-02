99c945f2e850c7e47ed0dae4d4afc9e290f29526
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-11-02 21:04:07 +0100

    Update New.py

--- New.py
+++ New.py
@@ -254,7 +254,7 @@ if st.session_state["authentication_status"]:
 	
 	
         if submit_button:
-            st.code("Thank you f"+"or contacting us!")
+            st.success('Thank you for contacting us!', icon="✅")
         chosen_id = stx.tab_bar(data=[
             stx.TabBarItemData(id="tab1", title="✍️ To Do", description="Tasks to take care of"),
             stx.TabBarItemData(id="tab2", title="📣 Done", description="Tasks taken care of"),
