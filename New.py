7f7f9ca70a2475dc2732936483389ba3cd0dd45f
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-11-02 17:49:12 +0100

    Update New.py

--- New.py
+++ New.py
@@ -169,12 +169,12 @@ if st.session_state["authentication_status"]:
      #       container.markdown(f'<h1 style="color:#de4b4b;font-size:15px;">{"Please insert input!"}</h1>', unsafe_allow_html=True)
 
         # Create API client.
-        credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"])
-        client = bigquery.Client(credentials=credentials)
+	 credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"])
+         client = bigquery.Client(credentials=credentials)
 
 
 
-        if ('Time1' not in st.session_state) and ('Time2' not in st.session_state):
+         if ('Time1' not in st.session_state) and ('Time2' not in st.session_state):
             query_time1="SELECT Date FROM `showheroes-bi.bi.bi_adstxt_join_sellerjson_with_count_domains` limit 1"
             df_time1= client.query(query_time1).to_dataframe()
             st.session_state['Time1']=df_time1['Date'][0]
@@ -185,8 +185,8 @@ if st.session_state["authentication_status"]:
 
 	
 
-        @st.cache(max_entries=1)
-        def load_data1(time): 
+         @st.cache(max_entries=1)
+         def load_data1(time): 
             query1="SELECT * except(Date) FROM `showheroes-bi.bi.bi_adstxt_join_sellerjson_with_count_domains`"
             query_job1 = client.query(query1)
             return client.query(query1).to_dataframe().fillna('-')
@@ -194,17 +194,17 @@ if st.session_state["authentication_status"]:
 
 
 
-        @st.cache(max_entries=1)
-        def load_data2(time):
+         @st.cache(max_entries=1)
+         def load_data2(time):
             query2="SELECT * except(Date) FROM `showheroes-bi.bi.bi_appadstxt_join_sellersjson_with_count_domains`"
             query_job2 = client.query(query2)
             return client.query(query2).to_dataframe().fillna('-')
 	
-        df1=load_data1(st.session_state['Time1']).copy()
-        df2=load_data2(st.session_state['Time2']).copy()
+         df1=load_data1(st.session_state['Time1']).copy()
+         df2=load_data2(st.session_state['Time2']).copy()
 
 
-        if (choice=="WEB") and (uploaded_file is not None):
+         if (choice=="WEB") and (uploaded_file is not None):
             # first filter before looping
             df1=df1[(df1['AdvertisingSystem'].isin(upload_input[0])) & (df1['PubAccId'].isin(upload_input[1]))]
             df1=df1.reset_index(drop=True)
@@ -219,7 +219,7 @@ if st.session_state["authentication_status"]:
             # Download 	
             download(data1)
 	
-        elif (choice=="WEB") and (list_lines!='Ex: google.com, 12335, DIRECT') and (list_lines.strip()!=''):
+         elif (choice=="WEB") and (list_lines!='Ex: google.com, 12335, DIRECT') and (list_lines.strip()!=''):
             # first filter 
             df1=df1[(df1['AdvertisingSystem'].isin(input[0])) & (df1['PubAccId'].isin(input[1]))]
             df1=df1.reset_index(drop=True)
@@ -233,8 +233,7 @@ if st.session_state["authentication_status"]:
             # Download 
             download(data1)
     
-	
-        elif (choice=="APP") and (uploaded_file is not None):   
+         elif (choice=="APP") and (uploaded_file is not None):   
             # first filter 
             df2=df2[(df2['AdvertisingSystem'].isin(upload_input[0])) & (df2['PubAccId'].isin(upload_input[1]))]
             df2=df2.reset_index(drop=True)
@@ -251,7 +250,7 @@ if st.session_state["authentication_status"]:
             download(data2)
 
 	
-        elif (choice=="APP") and (list_lines!='Ex: google.com, 12335, DIRECT') and (list_lines.strip()!=''):
+         elif (choice=="APP") and (list_lines!='Ex: google.com, 12335, DIRECT') and (list_lines.strip()!=''):
             # first filter
             df2=df2[(df2['AdvertisingSystem'].isin(input[0])) & (df2['PubAccId'].isin(input[1]))]
             df2=df2.reset_index(drop=True)
@@ -264,7 +263,7 @@ if st.session_state["authentication_status"]:
             # Download
             download(data2)	
     
-    elif st.session_state['authentication_status'] == False:
+     elif st.session_state['authentication_status'] == False:
         col04, col05,col06 = st.columns(3)
         with col04:
             st.write('')
@@ -272,7 +271,7 @@ if st.session_state["authentication_status"]:
             st.error('Username/password is incorrect')
         with col06:
             st.write('')
-    elif st.session_state['authentication_status'] == None:
+     elif st.session_state['authentication_status'] == None:
         col07, col08,col09 = st.columns(3)
         with col07:
             st.write('')
@@ -281,6 +280,6 @@ if st.session_state["authentication_status"]:
         with col09:
             st.write('')
     	 	
-    with tab2:
+     with tab2:
         st.header("A dog")
  #       st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
