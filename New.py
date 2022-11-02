a3f25e28224414116f9f13745f5d216f8a9f36ca
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-11-02 18:26:29 +0100

    Update New.py

--- New.py
+++ New.py
@@ -12,7 +12,6 @@ import time
 
 st.set_page_config(layout="wide")
 container=st.container()
-container2=st.container()
 
 # col=0 (advertisingsystem), 1 (PubAccId) , 2 (Relationship),  
 def check(df,col,keyword):
@@ -58,7 +57,8 @@ def download(output_data):
         st.write('No output found')
 	
 
-    
+	
+	
 	
     
 
@@ -142,10 +142,46 @@ if st.session_state["authentication_status"]:
     with col6:
        authenticator.logout('Logout', 'main')
 
-   
-    tab1, tab2= container.tabs(["Main", "Contact"])
-    with tab1:
-        if (choice=="WEB") and (uploaded_file is not None):
+    if (uploaded_file is None) and ((list_lines=='Ex: google.com, 12335, DIRECT') or (list_lines.strip()=='')):
+        container.markdown(f'<h1 style="color:#de4b4b;font-size:15px;">{"Please insert input!"}</h1>', unsafe_allow_html=True)
+
+    # Create API client.
+    credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"])
+    client = bigquery.Client(credentials=credentials)
+
+
+
+    if ('Time1' not in st.session_state) and ('Time2' not in st.session_state):
+        query_time1="SELECT Date FROM `showheroes-bi.bi.bi_adstxt_join_sellerjson_with_count_domains` limit 1"
+        df_time1= client.query(query_time1).to_dataframe()
+        st.session_state['Time1']=df_time1['Date'][0]
+
+        query_time2="SELECT Date FROM `showheroes-bi.bi.bi_appadstxt_join_sellersjson_with_count_domains` limit 1"
+        df_time2= client.query(query_time2).to_dataframe()
+        st.session_state['Time2']=df_time2['Date'][0]
+
+	
+
+    @st.cache(max_entries=1)
+    def load_data1(time): 
+        query1="SELECT * except(Date) FROM `showheroes-bi.bi.bi_adstxt_join_sellerjson_with_count_domains`"
+        query_job1 = client.query(query1)
+        return client.query(query1).to_dataframe().fillna('-')
+
+
+
+
+    @st.cache(max_entries=1)
+    def load_data2(time):
+        query2="SELECT * except(Date) FROM `showheroes-bi.bi.bi_appadstxt_join_sellersjson_with_count_domains`"
+        query_job2 = client.query(query2)
+        return client.query(query2).to_dataframe().fillna('-')
+	
+    df1=load_data1(st.session_state['Time1']).copy()
+    df2=load_data2(st.session_state['Time2']).copy()
+
+
+    if (choice=="WEB") and (uploaded_file is not None):
         # first filter before looping
         df1=df1[(df1['AdvertisingSystem'].isin(upload_input[0])) & (df1['PubAccId'].isin(upload_input[1]))]
         df1=df1.reset_index(drop=True)
@@ -204,48 +240,6 @@ if st.session_state["authentication_status"]:
 
         # Download
         download(data2)	
-	
-    with tab2:
-        st.header("A dog")
-    if (uploaded_file is None) and ((list_lines=='Ex: google.com, 12335, DIRECT') or (list_lines.strip()=='')):
-       container.markdown(f'<h1 style="color:#de4b4b;font-size:15px;">{"Please insert input!"}</h1>', unsafe_allow_html=True)
-    
-    # Create API client.
-    credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"])
-    client = bigquery.Client(credentials=credentials)
-
-
-
-    if ('Time1' not in st.session_state) and ('Time2' not in st.session_state):
-        query_time1="SELECT Date FROM `showheroes-bi.bi.bi_adstxt_join_sellerjson_with_count_domains` limit 1"
-        df_time1= client.query(query_time1).to_dataframe()
-        st.session_state['Time1']=df_time1['Date'][0]
-
-        query_time2="SELECT Date FROM `showheroes-bi.bi.bi_appadstxt_join_sellersjson_with_count_domains` limit 1"
-        df_time2= client.query(query_time2).to_dataframe()
-        st.session_state['Time2']=df_time2['Date'][0]
-
-	
-
-    @st.cache(max_entries=1)
-    def load_data1(time): 
-        query1="SELECT * except(Date) FROM `showheroes-bi.bi.bi_adstxt_join_sellerjson_with_count_domains`"
-        query_job1 = client.query(query1)
-        return client.query(query1).to_dataframe().fillna('-')
-
-
-
-
-    @st.cache(max_entries=1)
-    def load_data2(time):
-        query2="SELECT * except(Date) FROM `showheroes-bi.bi.bi_appadstxt_join_sellersjson_with_count_domains`"
-        query_job2 = client.query(query2)
-        return client.query(query2).to_dataframe().fillna('-')
-	
-    df1=load_data1(st.session_state['Time1']).copy()
-    df2=load_data2(st.session_state['Time2']).copy()
-
-
     
 elif st.session_state['authentication_status'] == False:
     col04, col05,col06 = st.columns(3)
