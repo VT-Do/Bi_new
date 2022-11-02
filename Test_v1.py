b85affb8fbad0459e7fe3bc96d9b53a73bd3d217
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-11-02 13:41:49 +0100

    Update Test_v1.py

--- Test_v1.py
+++ Test_v1.py
@@ -17,6 +17,21 @@ with st.empty():
         time.sleep(1)
     st.write("✔️ 1 minute over!")
 
+placeholder = st.empty()
+
+# Replace the placeholder with some text:
+placeholder.text("Hello")
+
+# Replace the text with a chart:
+placeholder.line_chart({"data": [1, 5, 2, 6]})
+
+# Replace the chart with several elements:
+with placeholder.container():
+    st.write("This is one element")
+    st.write("This is another")
+
+# Clear all those elements:
+placeholder.empty()
 	
 with open('config.yaml') as file:
     config = yaml.safe_load(file)
