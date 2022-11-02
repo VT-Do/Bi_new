44392ee34caf19a57eb29c770c3e5e533271765b
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-11-02 16:42:44 +0100

    Update New.py

--- New.py
+++ New.py
@@ -96,13 +96,15 @@ list_lines='Ex: google.com, 12335, DIRECT'
 
 
 if st.session_state["authentication_status"]:
-    choice = st.sidebar.radio("Select invironment",('WEB','APP'), horizontal=True)
+	
+    
+    choice = container2.sidebar.radio("Select invironment",('WEB','APP'), horizontal=True)
 
 
-    choice2 = st.sidebar.radio("Insert input",('Upload','Type/Paste'), horizontal=True)
+    choice2 = container2.sidebar.radio("Insert input",('Upload','Type/Paste'), horizontal=True)
 
     if choice2=='Upload':
-        uploaded_file = st.sidebar.file_uploader("Choose a .csv file")
+        uploaded_file = container2.sidebar.file_uploader("Choose a .csv file")
 
         if uploaded_file is not None:
             bytes_data = uploaded_file.getvalue()
