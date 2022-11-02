1c52d02f841823f2a104c1ef83158eef6d67d9cd
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-11-02 15:45:29 +0100

    Update New.py

--- New.py
+++ New.py
@@ -13,6 +13,7 @@ import time
 st.set_page_config(layout="wide")
 container=st.container()
 container2=st.container()
+container3=st.container()
 
 # col=0 (advertisingsystem), 1 (PubAccId) , 2 (Relationship),  
 def check(df,col,keyword):
@@ -64,7 +65,7 @@ def download(output_data):
     
 
 
-col01, col02,col03 = container2.columns(3)
+col01, col02,col03 = container3.columns(3)
 with col01:
     st.write('')
 with col02:
@@ -148,7 +149,7 @@ if st.session_state["authentication_status"]:
 
 
     if (uploaded_file is None) and ((list_lines=='Ex: google.com, 12335, DIRECT') or (list_lines.strip()=='')):
-        container.markdown(f'<h1 style="color:#de4b4b;font-size:15px;">{"Please insert input!"}</h1>', unsafe_allow_html=True)
+        container2.markdown(f'<h1 style="color:#de4b4b;font-size:15px;">{"Please insert input!"}</h1>', unsafe_allow_html=True)
 
     # Create API client.
     credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"])
