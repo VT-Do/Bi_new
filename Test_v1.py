ace03cd08a6603cb3e8bd02f301d50a8a5cc1c1c
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-11-02 13:25:25 +0100

    Update Test_v1.py

--- Test_v1.py
+++ Test_v1.py
@@ -12,7 +12,7 @@ st.set_page_config(layout="wide")
 import time
 
 with st.empty():
-    for seconds in range(60):
+    for seconds in range(10):
         st.write(f"⏳ {seconds} seconds have passed")
         time.sleep(1)
     st.write("✔️ 1 minute over!")
