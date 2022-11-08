2c39a2a2b75be03feb58dd29e159efe4390981fb
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-11-08 18:38:31 +0100

    Update New.py

--- New.py
+++ New.py
@@ -45,7 +45,7 @@ def check_row(df,input_data,row):
 # Similarity
 def similarity():
     with st.sidebar.expander("Similarity setting"):
-        similarity = st.sidebar.slider("Similarity level",min_value=0.0, max_value=1.0,value=1.0, step=0.1, help="You can choose the similarity level between 0 and 1, where 1 means indentical.",
+        similarity = st.slider("Similarity level",min_value=0.0, max_value=1.0,value=1.0, step=0.1, help="You can choose the similarity level between 0 and 1, where 1 means indentical.",
         )
         col001, col002,col003 =st.columns(3)
         with col001:
