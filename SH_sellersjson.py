c22ca3bbdecc592586492ef8e55b2efe4ecc6f82
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-11-23 09:41:37 +0100

    Create SH_sellersjson.py

---
+++ SH_sellersjson.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+from google.oauth2 import service_account
+from google.cloud import bigquery
+from io import StringIO
+import streamlit_authenticator as stauth
+import yaml
+import time
+import extra_streamlit_components as stx
+import smtplib
+from difflib import SequenceMatcher
+
+st.set_page_config(layout="wide")
+container=st.container()
+
+col4, col5,col6 = container.columns((4, 7, 1))
+
+with col4:
+   st.image("images.png", width=80)
+
+with col5:
+   st.title("📊 IAB dataset") 
+with col6:
+   authenticator.logout('Logout', 'main')
+    
+	
+
+	
+
+
+
+
