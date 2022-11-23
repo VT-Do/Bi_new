fa2d45df23358b18728d09d9f1c9cca1afb3a865
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-11-23 10:13:46 +0100

    Update SH_sellersjson.py

--- SH_sellersjson.py
+++ SH_sellersjson.py
@@ -17,13 +17,13 @@ from difflib import SequenceMatcher
 st.set_page_config(layout="wide")
 container=st.container()
 
-col4, col5,col6 = container.columns((1, 4, 1))
+col4, col5,col6 = container.columns((1, 5, 1))
 
 with col4:
    st.image("images.png", width=80)
 
 with col5:
-   st.title("📊 Showheroes SellersJson ") 
+   st.title("Showheroes SellersJson ") 
 with col6:
    st.write('')
     
