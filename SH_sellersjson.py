1b8701913221bd4c533fb0492ce6e77a0a8b41c7
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-11-23 10:14:06 +0100

    Update SH_sellersjson.py

--- SH_sellersjson.py
+++ SH_sellersjson.py
@@ -17,7 +17,7 @@ from difflib import SequenceMatcher
 st.set_page_config(layout="wide")
 container=st.container()
 
-col4, col5,col6 = container.columns((1, 5, 1))
+col4, col5,col6 = container.columns((1, 3, 1))
 
 with col4:
    st.image("images.png", width=80)
