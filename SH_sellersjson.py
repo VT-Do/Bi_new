62dc164254af00a17d49ede4c7296c6e9d2f60e5
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-12-02 15:23:54 +0100

    Update SH_sellersjson.py

--- SH_sellersjson.py
+++ SH_sellersjson.py
@@ -92,15 +92,13 @@ date=time()
 
 if st.sidebar.button('Update'):
     if date!=date.today():
-        st.sidebar.write('It takes time, please be patient')
-
-        
-      
+        st.sidebar.write('It takes time, please be patient') 
         df=load_data().copy()
 else:
     st.sidebar.write('last update', date)
     
     
 df=load_data().copy()
+
 st.write(check('sellers',df['url'][0])
 st.dataframe(df)
