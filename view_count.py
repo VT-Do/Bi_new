97d5151ea98d59da5e2054278d9c24fa1cb74f3e
VTDo <115161122+VT-Do@users.noreply.github.com>
2023-08-07 17:08:33 +0200

    Update view_count.py

--- view_count.py
+++ view_count.py
@@ -1,22 +1,16 @@
 import streamlit as st
+from streamlit import SessionState
 
-# Initialize view count
-view_count = 0
-
-# Check if 'view_count' exists in Streamlit's session state
-if 'view_count' not in st.session_state:
-    st.session_state.view_count = view_count
-
-# Increment view count when the app is accessed
-st.session_state.view_count += 1
-
-# Streamlit app layout
 def main():
     st.title("Streamlit View Count App")
-    
-    st.write("This app keeps track of the number of views.")
-    
-    st.write(f"Current view count: {st.session_state.view_count}")
+
+    # Initialize the SessionState class with a default value for view_count
+    session_state = SessionState.get(view_count=0)
+
+    # Increment the view count when the app is accessed
+    session_state.view_count += 1
+
+    st.write(f"Current view count: {session_state.view_count}")
 
 if __name__ == "__main__":
     main()
